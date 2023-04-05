3596017f9b3859ca2a8f4b49506389af0fd011c0
rudenkogh <116075187+rudenkogh@users.noreply.github.com>
2023-04-05 16:07:02 +0200

    Initial Commit

---
+++ XylophoneApp/XylophoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XylophoneAppApp.swift
+//  XylophoneApp
+//
+//  Created by A A on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct XylophoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
